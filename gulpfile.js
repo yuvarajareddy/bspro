d6e5e9aa1362f56a69e9b8d09ee4c3e8d55d0faf
Yuvaraja Reddy Nandarapu <yuvnanda@publicisgroupe.net>
2018-07-11 12:09:41 +0530

    Alpha version Bootstrap responsiveness.

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync =  require('browser-sync');
+//compile sass to css and auto inject to browsers to src â€œnode-modulesâ€ folder 
+gulp.task('sass',function(){
+gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss', './scss/styles.scss', './node_modules/bootstrap/scss/*.scss'])
+.pipe(sass({includePaths: ['scss']}))
+.pipe(gulp.dest('./css'))
+});
+
+
+gulp.task('browser-sync', function() {  
+    browserSync.init(['./css/*.css', './js/*.js'], {
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+
+gulp.task('default', ['sass', 'browser-sync'], function () {  
+    gulp.watch('./scss/*.scss', ['sass']);
+});
